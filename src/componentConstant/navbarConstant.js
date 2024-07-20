7715a4b2554538149435065cd12379d062baa80b
kaustav-karan <kaustavkaran2015@gmail.com>
2024-07-20 15:25:00 +0530

    navbar building in progress dont deply

---
+++ src/componentConstant/navbarConstant.js
@@ -0,0 +1,27 @@
+export const PROTECTEDNAVBARITEM = {
+  events: "events",
+  sponsor: "sponsor",
+  about: "about",
+  gallery: "gallery",
+};
+
+export const PROTECTEDNAVBARITEMARRAY = [
+  { id: protectedNavbarItems.about, title: "About", href: "/about" },
+  { id: protectedNavbarItems.events, title: "Events", href: "/events" },
+  { id: protectedNavbarItems.sponsor, title: "Sponsor", href: "/sponsor" },
+  { id: protectedNavbarItems.gallery, title: "Gallery", href: "/gallery" },
+];
+
+export const PUBLICNAVBARITEM = {
+    about: "about",
+    events: "events",
+    gallery: "gallery",
+    sponsor: "sponsor",
+};
+
+export const PUBLICNAVBARITEMARRAY = [
+    { id: PUBLICNAVBARITEM.about, title: "About", href: "/about" },
+    { id: PUBLICNAVBARITEM.events, title: "Events", href: "/events" },
+    { id: PUBLICNAVBARITEM.sponsor, title: "Sponsor", href: "/sponsor" },
+    { id: PUBLICNAVBARITEM.gallery, title: "Gallery", href: "/gallery" },
+];
