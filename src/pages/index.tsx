96b2433bba994dfaeff0896293b9eed72f1f61c1
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2024-07-29 23:57:47 +0530

    FAQ Accordion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero/Hero";
 import { SponsorMarquee } from "@/components/Marquee/marquee";
 import React from "react";
 import Layout from "../components/Layout";
+import { AccordionFAQ } from "@/components/Accordion/accordion";
 
 const Home: React.FC = () => {
   return (
     <Layout>
       <Hero />
       <SponsorMarquee />
+      <AccordionFAQ />
     </Layout>
   );
 };
