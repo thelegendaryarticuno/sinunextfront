5a6a44a1a0c11ffed2865971c51b267c26ee6df0
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2024-08-11 14:51:22 +0530

    Update merchandise.tsx

--- src/pages/merchandise.tsx
+++ src/pages/merchandise.tsx
@@ -1,8 +1,6 @@
 import Hero from "@/components/Hero/Hero";
 import React from "react";
 import Layout from "../components/ui/Layout";
-import MerchBanner from "@/components/MerchBanner/MerchBanner";
-
 
 const Home: React.FC = () => {
   return (
