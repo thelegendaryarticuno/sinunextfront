36b82f6cbdd445af9ce31366966c122f197f39eb
Naman Mani <naman.mani23@st.niituniversity.in>
2024-08-18 15:16:55 +0530

    SEO POSITIONING

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,26 +7,26 @@ import AboutHero from "@/components/abouthero/hero";
 import MeetTheTeam from "@/components/aboutgrid/ProfileCard";
 
 const About: React.FC = () => {
+  
   return (
     <>
+    <SEOComponent
+      PageDescription="siNUsoid had its maiden appearance in 2016. Rooted in the legacy of innovation, each edition delves deeper into groundbreaking themes, pushing the boundaries of tech exploration. Join us at siNUsoid to experience a journey from learning to real-world application, where history meets the future."
+      PageKeywords={[
+        "sinusoid",
+        "techfest",
+        "about",
+        "team",
+        "niit university",
+        "legacy",
+        "innovation",
+      ]}
+      PageOGLImage="/logo/logo.png"
+      PageTitle="About Us | siNUsoid v8"
+    />
     <AboutHero/>
     <AboutHistory/>
     <MeetTheTeam/>
-      <SEOComponent
-        PageDescription="siNUsoid had its maiden appearance in 2016. Rooted in the legacy of innovation, each edition delves deeper into groundbreaking themes, pushing the boundaries of tech exploration. Join us at siNUsoid to experience a journey from learning to real-world application, where history meets the future."
-        PageKeywords={[
-          "sinusoid",
-          "techfest",
-          "about",
-          "team",
-          "niit university",
-          "legacy",
-          "innovation",
-        ]}
-        PageOGLImage="/logo/logo.png"
-        PageTitle="About Us | siNUsoid v8"
-      />
-    
     
     <Our/>
     </>
