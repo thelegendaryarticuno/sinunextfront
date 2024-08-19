79b96e270512a6e73ce5e5d34352625457ab6018
Kinshukaumhum <kaustavkaran2015@gmail.com>
2024-08-19 21:29:59 +0530

    refactor: Update collaboration section in collab.tsx
    - Update the color of the collaboration section based on the theme (dark or light)
    - Remove unused imports and reformat code for better readability

--- src/components/abouthistory/collab.tsx
+++ src/components/abouthistory/collab.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { useTheme } from 'next-themes';
-import SlideShow from '../aboutimage/image';
-import Particles from '../magicui/particles';
+import React from "react";
+import { useTheme } from "next-themes";
+import SlideShow from "../aboutimage/image";
+import Particles from "@/components/magicui/particles";
 
 type CollaborationSectionProps = {
   imageSrc: string;
   text: string;
 };
 
-const CollaborationSection: React.FC<CollaborationSectionProps> = ({ imageSrc, text }) => {
+const CollaborationSection: React.FC<CollaborationSectionProps> = ({
+  imageSrc,
+  text,
+}) => {
   const { theme, setTheme } = useTheme();
 
   return (
     <div className="relative min-h-[40vh] py-4  flex items-center justify-center px-4 sm:px-8">
       <div className="relative flex-1 py-4 flex flex-col justify-between dark:bg-zinc-950 bg-zinc-200 bg-opacity-50 backdrop-blur-md rounded-xl shadow-lg">
-      <Particles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color={theme === "dark" ? "#ffffff" : "#000000"}
-        refresh
-      />
+        <Particles
+          className="absolute inset-0 "
+          quantity={100}
+          ease={80}
+          color={theme === "dark" ? "#ffffff" : "#000000"}
+          refresh
+        />
         <h3 className="dark:text-white text-black text-3xl md:text-5xl font-bold mb-6 text-center">
           History of siNUsoid
         </h3>
