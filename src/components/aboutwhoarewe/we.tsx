50054569a4f68ed4550a8579076fc61ac16d6be1
Naman Mani <naman.mani23@st.niituniversity.in>
2024-08-19 20:51:10 +0530

    updated content

--- src/components/aboutwhoarewe/we.tsx
+++ src/components/aboutwhoarewe/we.tsx
@@ -12,14 +12,14 @@ const We: React.FC<WeProps> = ({ imageSrc, title, description, principles }) => 
   return (
     <div className="flex flex-col md:flex-row items-center justify-center p-6 md:p-12">
       <div className="md:w-1/2 flex justify-center">
-        <div className="relative w-full h-full max-w-md">
+        <div className="relative w-full h-full max-w-[32rem]">
         <Image
             src={imageSrc}
             alt="Group of people"
             layout="responsive"
             width={800}
             height={500}
-            className="rounded-3xl w-[32rem]"
+            className="rounded-3xl"
           />
         </div>
       </div>
@@ -42,14 +42,14 @@ const Our: React.FC = () => {
     <We
       imageSrc="/images/we.png"  
       title="What we're all about:"
-      description="siNUsoid isn't your average tech fest. It's a wild ride through the coolest corners of technology! We are all about bringing together future innovators, tech geeks and those seeking knowledge. We're here to help you celebrate technological advancements with the help of mind-blowing pre-fest events and thrilling on-fest (events/competitions), all under the theme for each year's edition. And believe us when we say, there's something for everyone here. So what are you waiting for? Come explore the awesome world of tech and create truly amazing memories together with siNUsoid v8."
+      description="siNUsoid is far from your average tech fest. It's a wild ride through the coolest corners of technology, bringing together future innovators, tech geeks and those seeking knowledge. We celebrate technological advancements with the help of mind-blowing pre-fest events and thrilling on-fest events, all under the theme for each year's edition. And believe us when we say, there's something for everyone here. So why wait? Come explore the awesome world of tech and create truly amazing memories together with siNUsoid v8."
       principles={[
         "Embrace every fail; it's not the end of the world",
         "Celebrate the nerds; if we don't, who will?",
         "Dream big; shoot for the stars!",
         "Boring is banned; we're here to have a good time!",
         "Peace, love and plants ☘️ <3",
-        "Inclusivity is key, we definitely support everyone wink",
+        "Inclusivity is key, we definitely support everyone (wink)",
         "Touch grass, feel the air on your face!"
       ]}
     />
