01f250a2c7393ae9dc7d1689c0b24f5403d2b762
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2024-08-19 21:14:11 +0530

    titleSponsor and coSponsor ShineBorder

--- src/components/abouthistory/collab.tsx
+++ src/components/abouthistory/collab.tsx
@@ -13,14 +13,15 @@ const CollaborationSection: React.FC<CollaborationSectionProps> = ({ imageSrc, t
 
   return (
     <div className="relative min-h-[40vh] py-4 dark:bg-black bg-white flex items-center justify-center px-4 sm:px-8">
+
       <div className="relative flex-1 py-4 flex flex-col justify-between dark:bg-zinc-950 bg-zinc-200 bg-opacity-50 backdrop-blur-md rounded-xl shadow-lg">
-      <Particles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color={theme === "dark" ? "#ffffff" : "#000000"}
-        refresh
-      />
+         <Particles
+          className="absolute inset-0"
+          quantity={100}
+          ease={80}
+          color={theme === "dark" ? "#ffffff" : "#000000"}
+          refresh
+        />
         <h3 className="dark:text-white text-black text-3xl md:text-5xl font-bold mb-6 text-center">
           History of siNUsoid
         </h3>
--- src/pages/sponsor.tsx
+++ src/pages/sponsor.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SponsorPageMarquee } from "@/components/SponsorPageMarquee/SponsorPageMarquee";
 import SEOComponent from "@/components/SEOComponent/SEOComponent";
 import Image from "next/image";
-import Link from "next/link";
+import ShineBorder from "@/components/magicui/shine-border";
 
 const Sponsors: React.FC = () => {
   return (
@@ -41,12 +41,15 @@ const Sponsors: React.FC = () => {
           </div>
         </div>
         <SponsorPageMarquee />
-        
-        
         <div className="relative min-h-[50vh] dark:bg-black bg-white flex items-center justify-center p-4">
-          <div className="max-w-4xl w-full dark:bg-zinc-900 bg-zinc-200 rounded-lg shadow-3xl overflow-hidden flex flex-col md:flex-row">
-            <div className="relative w-full md:w-[30%] flex items-center justify-center md:justify-end p-4 md:border-r-2 border-zinc-400 dark:border-zinc-700">
-              <div className="relative w-full h-[20vh] md:h-auto flex items-center justify-center">
+          <ShineBorder className="max-w-4xl w-full dark:bg-zinc-900 bg-zinc-200 rounded-lg shadow-3xl overflow-hidden flex flex-col md:flex-row" color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}>
+            <div className="relative w-full md:w-[7%] flex items-center justify-center p-4">
+              <div className="text-sm md:text-base dark:text-white text-black" style={{writingMode: "vertical-lr", textOrientation: "upright"}}>
+                TITLE SPONSOR
+              </div>
+            </div>
+            <div className="relative w-full md:w-[30%] flex flex-col items-center justify-center md:border-l-4 border-zinc-50 dark:border-zinc-950">
+              <div>
                 <Image 
                   src="/images/logo_red_mc.png" 
                   alt="siNUsoid Logo" 
@@ -57,14 +60,40 @@ const Sponsors: React.FC = () => {
               </div>
             </div>
             <div className="flex-1 p-4 md:p-8 flex flex-col justify-between md:ml-4">
-              <h1 className="dark:text-white text-black text-4xl md:text-5xl font-bold mb-6">
+              <div className="dark:text-white text-black text-4xl md:text-5xl font-bold mb-6">
                 About siNUsoid
-              </h1>
-              <h3 className="dark:text-white text-black mb-6 font-bold text-base">
+              </div>
+              <div className="dark:text-white text-black mb-6 font-bold text-base">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magnam deserunt laudantium doloremque minima, iste corporis quaerat animi blanditiis, iure fugiat nostrum eius modi ipsam ex culpa, quasi odit commodi.
-              </h3>
+              </div>
             </div>
-          </div>
+          </ShineBorder>
+        </div>
+        <div className="relative min-h-[50vh] dark:bg-black bg-white flex items-center justify-center p-4">
+          <ShineBorder className="max-w-4xl w-full dark:bg-zinc-900 bg-zinc-200 rounded-lg shadow-3xl overflow-hidden flex flex-col md:flex-row-reverse" color={["#00DBDE", "#FC00FF", "#50CC7F"]}>
+            <div className="relative w-full md:w-[7%] flex items-center justify-center p-4">
+              <div className="text-sm md:text-base dark:text-white text-black" style={{writingMode: "vertical-lr", textOrientation: "upright"}}>
+                Co SPONSOR
+              </div>
+            </div>
+            <div className="relative w-full md:w-[30%] flex flex-col items-center justify-center md:border-r-4 border-zinc-50 dark:border-zinc-950">
+                <Image 
+                  src="/images/logo_red_mc.png" 
+                  alt="siNUsoid Logo" 
+                  width={150}
+                  height={150}
+                  className="object-contain p-4 z-20"
+                />
+            </div>
+            <div className="flex-1 p-4 md:p-8 flex flex-col justify-between md:mr-4">
+              <div className="dark:text-white text-black text-4xl md:text-5xl font-bold mb-6">
+                About siNUsoid
+              </div>
+              <div className="dark:text-white text-black mb-6 font-bold text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum magnam deserunt laudantium doloremque minima, iste corporis quaerat animi blanditiis, iure fugiat nostrum eius modi ipsam ex culpa, quasi odit commodi.
+              </div>
+            </div>
+          </ShineBorder>
         </div>
       </div>
     </>
