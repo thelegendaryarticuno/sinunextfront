5c7382b48b84d84a1862b1e75e0dd3611b06847f
Kinshukaumhum <kaustavkaran2015@gmail.com>
2024-09-06 09:35:11 +0530

    remove unrequired depen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 // src/pages/_app.tsx
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import { ClerkProvider } from "@clerk/nextjs";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
@@ -11,13 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics gaId="GTM-58KNP2JR" />
-      <ClerkProvider>
         <ThemeProvider defaultTheme="dark" attribute="class">
           <Header />
           <Component {...pageProps} />
           <Footer />
         </ThemeProvider>
-      </ClerkProvider>
     </>
   );
 }
