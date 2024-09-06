9312f7bd889cb8ed0db6a053498bdeb38ec7f147
Kinshukaumhum <kaustavkaran2015@gmail.com>
2024-09-06 09:49:23 +0530

    Remove unnecessary dependency and update package.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
