a5c8cb7a50d0a91a273a598adb284ae9f69a11a5
Naman Mani <naman.mani23@st.niituniversity.in>
2024-09-16 23:59:02 +0530

    updated svg path

--- src/components/formikform/formikform.tsx
+++ src/components/formikform/formikform.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import * as Yup from "yup";
+import { FaSpinner } from "react-icons/fa"; // Importing the spinner icon
 
 interface FormField {
   name: string;
@@ -37,7 +38,10 @@ const FormikForm: React.FC<FormikFormProps> = ({
       {({ isSubmitting }) => (
         <Form className="grid gap-6 py-4">
           {fields.map((field) => (
-            <div className="grid grid-cols-4 items-center gap-6" key={field.name}>
+            <div
+              className="grid grid-cols-4 items-center gap-6"
+              key={field.name}
+            >
               <Label htmlFor={field.name} className="text-right">
                 {field.label}
               </Label>
@@ -58,21 +62,7 @@ const FormikForm: React.FC<FormikFormProps> = ({
 
           <Button type="submit" disabled={isSubmitting}>
             {isSubmitting ? (
-              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              <FaSpinner className="animate-spin h-5 w-5 mr-3" />
             ) : (
               submitButtonText
             )}
