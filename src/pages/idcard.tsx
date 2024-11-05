cf73441c054f1e8ea33e281455d1f463c00cb2cf
Zain Khan <142525731+Zain-Khan05@users.noreply.github.com>
2024-11-06 03:30:07 +0530

    idcard

--- src/pages/idcard.tsx
+++ src/pages/idcard.tsx
@@ -1,10 +1,12 @@
+import IdCard from '@/components/idcard/id'
 const Id = () => {
   return (
-    <div>
-      <h1>ID Card </h1>
-      <div>{/* Add your content here */}</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 p-4">
+      <div className="mt-12 mb-8"> 
+        <h1 className="text-white text-3xl text-center mb-4">ID Card</h1>
+        <IdCard name="Yashwanth" qrData="https://aaa.com" />
+      </div>
     </div>
-  );
-};
-
-export default Id;
+  )
+}
+export default Id
